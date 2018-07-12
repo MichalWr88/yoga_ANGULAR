5a44b095507c84cbf5f8295f10f21745195aa6b4
Michal <maleszewski.geo@gmail.com>
2018-07-12 07:17:23 +0200

    add google map

--- yoga/src/app/shared/footer/footer.component.ts
+++ yoga/src/app/shared/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  title: string = 'Belle & Carrie Rehabilitation Yoga';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  zoom: number = 14;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
